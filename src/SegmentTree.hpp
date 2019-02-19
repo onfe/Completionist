e390572feb496dc4092356f9544895ae2be67b15
Edward <onfe@users.noreply.github.com>
2019-02-19 13:41:06 +0000

    Move SegmentTreeNode to separate file.

--- src/SegmentTree.hpp
+++ src/SegmentTree.hpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "SegmentTreeNode.hpp"
 
 using namespace std;
 
@@ -18,22 +19,3 @@ private:
 
   }
 };
-
-class SegmentTreeNode {
-  int s, min, max, from, to, pval;
-  bool pending;
-public:
-  SegmentTreeNode() {
-    s = 0;
-    min = 0;
-    max = 0;
-    from = 0;
-    to = 0;
-    pval = 0;
-    pending = false;
-  }
-
-  int size() {
-    return to - from + 1;
-  }
-}
