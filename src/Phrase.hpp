c9ab6c259d1ae1fde6002d9bb0c5aad797526c36
Edward <onfe@users.noreply.github.com>
2019-02-21 12:57:59 +0000

    Fix operator with const.

--- src/Phrase.hpp
+++ src/Phrase.hpp
@@ -9,7 +9,7 @@ struct phrase_t {
 
   phrase_t(std::uint32_t w, std::string p) : weight(w), phrase(p) {}
 
-  bool operator<(phrase_t& rhs) {
+  bool operator<(phrase_t const &rhs) const {
     return this->phrase < rhs.phrase;
   }
 
